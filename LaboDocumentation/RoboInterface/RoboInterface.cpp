aa05dc2a780558a9d2acb41f29e3d606448cc877
nickwouters.work <nickwouters.work@gmail.com>
2015-03-04 10:28:16 +0000

    Interface cpp afgewerkt met lege stub voor move

---
+++ LaboDocumentation/RoboInterface/RoboInterface.cpp
@@ -0,0 +1,22 @@
+#include "Can.h"
+#include <iostream>
+
+RoboInterface::RoboInterface(int myDistance, int myAngle) {
+	distance = myDistance;
+	angle = myAngle;
+	collected = false;
+}
+
+void RoboInterface::setDistance(int myDistance){
+	distance = myDistance;
+}
+
+void RoboInterface::setAngle(int myAngle){
+	angle = myAngle;
+}
+
+void RoboInterface::setCollected(bool myCollected){
+	collected = myCollected;
+}
+void RoboInterface::move(){
+}
