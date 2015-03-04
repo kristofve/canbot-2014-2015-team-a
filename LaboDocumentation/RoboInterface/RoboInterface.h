2354a3f8f08d78904920a45ae45b81ffc5cb4aeb
nickwouters.work <nickwouters.work@gmail.com>
2015-03-04 10:27:44 +0000

    Interface header afgewerkt

---
+++ LaboDocumentation/RoboInterface/RoboInterface.h
@@ -0,0 +1,18 @@
+#include <iostream>
+
+class RoboInterface{
+	int distance;
+	int angle;
+	bool collected;
+
+	public:
+		RoboInterface(int,int);
+		int getDistance() {return distance;}
+		int getAngle() {return angle;}
+		bool isCollected() {return collected;}
+		void setDistance(int myDistance);
+		void setAngle(int myAngle);
+		void setCollected(bool myCollected);
+		void printCan() {std::cout<<"distance: " <<getDistance()<< " angle: " << getAngle()<< " isCollected? " << isCollected() << "\n";}
+};
+		void move();
