416ce9e7a9b30a6d2ac962a7b8e83182866aac3e
Kristof Van Engeland <kristofvanengeland@gmail.com>
2015-03-25 11:28:24 +0100

    copy script

--- copy_remote.sh
+++ copy_remote.sh
@@ -2,6 +2,6 @@
 
 #scp -r pi@192.168.1.11:~/CanbotTeamA/* ~/Documents/raspberry/canbot
 
-LOC_DIR="~/Documents/raspberry "
+LOC_DIR="~/Documents/raspberry"
 REM_DIR="~/CanbotTeamA/robo2015"
 rsync -avr -e ssh --include "*.cpp$" --include "*.h$" --include "Makefile$" --exclude "*.o" --exclude "*.swp" . pi@192.168.1.11:~/CanbotTeamA/robo2015
